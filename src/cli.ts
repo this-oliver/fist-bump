9fbb3feadb202e5447e68f1b6dc9d5850dde7990
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-09-25 22:30:32 +0200

    (v2.0.1) patch: minor refactor

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * @description A simple CLI to bump the version of a package.json file.
- */
+import { runFistBump } from "./index";
 
-import { bump } from "./index";
-
-bump();
+runFistBump();
